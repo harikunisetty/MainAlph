6a4bd6d30f4cef1d85d4a48c727cf11c33de6077
harikunisetty <86394723+harikunisetty@users.noreply.github.com>
2021-12-18 17:56:48 -0800

    update

---
+++ Assets/English Tracing Book/Scripts/BaloonMoving.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaloonMoving : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
